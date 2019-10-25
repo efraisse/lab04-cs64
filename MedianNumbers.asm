884fbb28b937e862a4195ff0afc5045968f78826
Erwan Fraisse <efraisse@cs.ucsb.edu>
2019-10-25 12:28:41 -0700

    trying this out

--- MedianNumbers.asm
+++ MedianNumbers.asm
@@ -4,19 +4,95 @@
 
 .data
 
-	# TODO: Complete these declarations / initializations
+# TODO: Complete these declarations / initializations
 
-	.asciiz "Enter the next number:\n"
+nextNum = .asciiz "Enter the next number:\n"
 
-	.asciiz "Median: "
-
+median = .asciiz "Median: "
 
 #Text Area (i.e. instructions)
 .text
 
 main:
 
-	# TODO: Write your code here
+    li $v0, 4
+    la $a0,nextNum
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t0, $v0
+
+    li $v0, 4
+    la $a0,nextNum
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t1, $v0
+
+    li $v0, 4
+    la $a0,nextNum
+    syscall
+
+    li $v0, 5
+    syscall
+    move $t2, $v0
+
+    blt $t0, $t1, t0LessThan
+    blt $t1, $t0, t1LessThan
+
+t0LessThan:
+    blt $t2, $t0, t0IsMedian
+    blt $t0, $t2, t2IsMedian
+
+t1LessThan:
+    blt $t2, $t1, t1IsMedian
+    blt $t1, $t2, t2IsMedian
+
+t0IsMedian:
+    li $v0, 4
+    la $a0, median
+    syscall
+    
+    li $v0, 1
+    move $a0, $t0
+    syscall
+    J exit
+
+t1IsMedian:
+
+    li $v0, 4
+    la $a0, median
+    syscall
+    
+    li $v0, 1
+    move $a0, $t1
+    syscall
+    J exit
+
+t2IsMedian:
+
+    li $v0, 4
+    la $a0, median
+    syscall
+    
+    li $v0, 1
+    move $a0, $t2
+    syscall
+    J exit
+
+    // if (t0 < t1) {
+    //    if (t1 < t2) {
+    //      cout << t1;
+    //    }
+    // } else if (t0 < t2) {
+    //   if (t0 > t1) {
+    //      cout << t0;
+    //   }
+    // } else {
+    //   if (t2 < t1) 
+    // }
 
 exit:
 	# Exit
