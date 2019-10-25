d64106e2a91b38bfc3e19d50f053fff39fe92a04
Erwan Fraisse <efraisse@cs.ucsb.edu>
2019-10-25 13:09:36 -0700

    done with part 2

--- factorial.asm
+++ factorial.asm
@@ -13,14 +13,48 @@
 #
 .data
 
-	# TODO: Write your initializations here
+phrase1: .asciiz "Enter the number:\n"
+phrase2: .asciiz "Factorial is:\n"
 
 #Text Area (i.e. instructions)
 .text
 main:
 
-	# TODO: Write your code here
+	li $v0, 4
+    la $a0, phrase1
+    syscall
 
+    li $v0, 5
+    syscall
+    move $t0, $v0
+    li $t3, 1
+    ble, $t0, $t3, print1
+    addi $t1, $t0, -1
+
+    li $v0, 4
+    la $a0, phrase2
+    syscall
+    j loop
+
+loop:
+    mult $t0, $t1
+    mflo $t0
+    addi $t1, $t1, -1
+    ble $t1, $t3, printFactorial
+    j loop
+
+printFactorial:
+    li $v0, 1
+    move $a0, $t0
+    syscall
+    j exit
+
+print1:
+    li $v0, 1
+    move $a0, $t3
+    syscall
+    j exit
+    
 exit:
 	# Exit
 	li $v0, 10
